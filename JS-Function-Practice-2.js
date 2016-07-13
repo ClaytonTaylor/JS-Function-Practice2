bd6dd043abc517c97fb19aa931732955a9c4239a
ClaytonTaylor <claytaylor@cox.net>
2016-07-13 16:53:49 -0600

    Function Practice 2

---
+++ JS-Function-Practice-2.js
@@ -0,0 +1,39 @@
+// #1
+
+/*
+
+function getName (name) {
+	var description = {
+		name: "luisa",
+		age: 25,	
+	};
+	return console.log(description.name)
+};
+
+getName();
+
+
+*/
+
+// #2
+
+/*
+
+var strings = ["Clay" , "Taylor"];
+
+function totalLetters (number) {
+	var count = 0;
+	for(var i = 0; i < strings.length; i++) {
+		count +=  (strings[i].length);
+	}
+
+	return count
+}
+
+totalLetters(strings);
+
+
+*/
+
+// #3
+
