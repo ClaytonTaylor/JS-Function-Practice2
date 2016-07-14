8cd30e47fc2fd55d147e2d539cb46faf17fc91ac
ClaytonTaylor <claytaylor@cox.net>
2016-07-14 14:35:40 -0600

    JS-Function-Practice-2 Continued

--- JS-Function-Practice-2.js
+++ JS-Function-Practice-2.js
@@ -37,3 +37,47 @@ totalLetters(strings);
 
 // #3
 
+/*
+
+function keyValue (city, name) {
+	var keyCity = "city: ";
+	var keyName = "Denver";
+	var Obj = {
+		name: keyCity,
+		city: keyName
+	}
+	return Obj.name + Obj.city
+}
+
+keyValue();
+
+*/
+
+// #4
+
+/*
+
+function negativeIndex (somearray, number) {
+	var array = ["apple", "betty", "cat", "dog", "egg"]
+	return array[array.length-3]
+}
+
+negativeIndex();
+
+*/
+
+// #5
+
+/*
+
+function removeM (string) {
+	var word = prompt ("Please enter a word with an 'M'.");
+	return word.replace('m','')
+	
+}
+removeM();
+
+*/
+
+// #6
+
